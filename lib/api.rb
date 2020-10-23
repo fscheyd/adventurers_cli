922b2dd3968082d0bf39793eb1f0e8d6c8bfe5ad
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-10-23 19:05:32 +0000

    array_of_beasts with iteration attribute added

--- lib/api.rb
+++ lib/api.rb
@@ -1,12 +1,34 @@
 class API
-
   def self.fetch_monsters
     url = "https://api.open5e.com/monsters/"
-    binding.pry
+
     uri = URI(url)
     response = Net::HTTP.get(uri)
     hash = JSON.parse(response)
 
-  end
 
+
+    array_of_beasts = hash["results"]
+
+
+
+    # "name" - name
+    # "size" - size
+    # "type" - type
+    # "alignment" - alignment
+    # "armor_class" - ac
+    # "hit_points" - hp
+    # :"name", :"size" ,:"alignment" ,:"armor_class" ,:"hit_points"
+
+    array_of_beasts.each do |monster_hash|
+      binding.pry
+      monster = Monsters.new
+        monster.name = monster_hash["name"]
+        monster.size = monster_hash["size"]
+        monster.type = monster_hash["type"]
+        monster.alignment = monster_hash["alignment"]
+        monster.armor_class = monster_hash["armor_class"]
+        monster.hit_points = monster_hash["hit_points"]
+    end
+  end
 end
--- lib/monsters.rb
+++ lib/monsters.rb
@@ -1,3 +1,13 @@
 class Monsters
+  attr_accessor :name, :size, :type ,:alignment ,:armor_class ,:hit_points
+
+  @@all = []
+    def initialize
+      @@all << self
+    end
+
+  def self.all
+      @@all
+  end
 
 end
